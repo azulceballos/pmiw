26e7274471d9c1084579c0c4b6eac560e7f52162
azulceballos <166521226+azulceballos@users.noreply.github.com>
2024-11-15 23:16:26 -0300

    Add files via upload

---
+++ tpfinalparte2/tpfinalparte2/tpfinalparte2.js
@@ -0,0 +1,26 @@
+//Victoria Di Lorenzo y Azul Ceballos
+//Comisión 1
+//https://youtu.be/_ePzKGYzi2k
+let juego;
+let sonidoFondo;
+
+function preload() {
+  fondoInicio = new Fondo("data/imagen5.jpeg");
+  fondoJuego = new Fondo("data/imagen.jpg");
+  sonidoFondo = loadSound("data/fondo.mp3");
+}
+
+function setup() {
+  createCanvas(640, 480);
+  juego = new Juego(); 
+}
+
+function draw() {
+  juego.mostrar();  
+}    
+
+function keyPressed() {
+  if (key === 'R' || key === 'r') {
+    juego.iniciarJuego(); // Reinicia el juego cuando se presiona 'R'
+  }
+}
